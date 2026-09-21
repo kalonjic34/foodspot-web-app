647df13ef4ca3f8c6bb70e893cb47bbd388c496f
Christian <kalonjic34@gmail.com>
2026-09-21 18:11:18 +0200

    add URL routing for foodspot_app in main urls.py

--- foodspot/urls.py
+++ foodspot/urls.py
@@ -19,5 +19,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("",include("workspace.urls"))
+    path("workspace/",include("workspace.urls")),
+    path("",include("foodspot_app.urls"))
+    
 ]
