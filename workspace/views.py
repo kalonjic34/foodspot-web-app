5ca29a91c2e6ee2baefcaf5e2371f198141bc157
Christian <kalonjic34@gmail.com>
2026-09-21 15:44:20 +0200

    implement index view to render food listing

--- workspace/views.py
+++ workspace/views.py
@@ -1,3 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    data = ["Pizza", "pasta", "bread", "salad", "jam", "sushi"]
+
+    context = {"foods":data}
+    return render(request,"workspace/index.html",context)
