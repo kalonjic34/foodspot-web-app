1fa77de5591beefd0846d8a40af21d4d9f45827c
Christian <kalonjic34@gmail.com>
2026-09-22 15:00:19 +0200

    add Recipe model definition with fields and string representation

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+from foodspot_app.models import Category
+
+class Recipe(models.Model):
+    name=models.CharField(max_length=100)
+    description=models.TextField(null=True, blank=True)
+    ingredients=models.TextField()
+    directions=models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.name
+    
+    
