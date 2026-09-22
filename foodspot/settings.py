7fb367010dd8640a9c7ea11735e1a1ed01f6c3d0
Christian <kalonjic34@gmail.com>
2026-09-22 15:30:18 +0200

    add comments app to INSTALLED_APPS in settings

--- foodspot/settings.py
+++ foodspot/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'workspace',
     'foodspot_app',
     'recipes',
+    'comments',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
