227f599ca600ffd80ff2b1eb610630377c46ad6a
Christian <kalonjic34@gmail.com>
2026-09-23 10:28:08 +0200

    add recipe detail and list views with corresponding templates

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,16 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Recipe
+
+def recipes(request):
+    recipes = Recipe.objects.all()
+    context = {"recipes":recipes}
+    return render(request,"recipes/recipes.html",context)
+
+def recipe(request, recipe_id):
+    recipe= Recipe.objects.get(id=recipe_id)
+    context = {
+        "recipe":recipe
+    }
+    return render(request, "recipes/recipe.html",context)
