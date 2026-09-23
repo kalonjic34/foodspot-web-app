f8a1f7851482f98d7e10747f20d839d7f6a9bfe8
Christian <kalonjic34@gmail.com>
2026-09-23 14:34:53 +0200

    add forms for category and recipe management

---
+++ foodspot_app/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+
+from foodspot_app.models import Category
+from recipes.models import Recipe
+
+
+class CategoryForm(forms.ModelForm):
+    class Meta:
+        model=Category
+        fields=["name"]
+        labels={"name":"Category Name"}
+class RecipeForm(forms.ModelForm):
+    class Meta:
+        model = Recipe
+        fields = ["name","description","ingredients","directions","category"]
+        
+         
