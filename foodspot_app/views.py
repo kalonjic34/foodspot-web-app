1ab36bc3cba8b14a550f8b0343eeed1210814bfd
Christian <kalonjic34@gmail.com>
2026-09-23 14:35:57 +0200

    refactor views to include redirect functionality and improve category and recipe handling

--- foodspot_app/views.py
+++ foodspot_app/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+
+from foodspot_app.forms import CategoryForm, RecipeForm
 from .models import Category
 from recipes.models import Recipe
 
@@ -12,3 +14,26 @@ def recipes(request, category_id):
     category= Category.objects.get(pk=category_id)
     context={"recipes":recipes, "category":category}
     return render(request,"foodspot_app/recipes.html",context)
+
+def add_category(request):
+    if request.method =="POST":
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("foodspot_app:index")
+        else:
+            return render(request, "foodspot_app/add_category.html",context)
+    else:
+        form=CategoryForm()
+        context={"form":form}
+        return render(request, "foodspot_app/add_category.html",context)
+def add_recipe(request):
+    if request.method == "POST":
+        form = RecipeForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("foodspot_app:index")
+    else:
+        form = RecipeForm()
+
+    return render(request, "foodspot_app/add_recipe.html", {"form": form})
