2d32f563a61672710f38f1e577d8cc4674397d89
Christian <kalonjic34@gmail.com>
2026-09-23 11:19:32 +0200

    add context and render logic to index and recipes views

--- foodspot_app/views.py
+++ foodspot_app/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
+from .models import Category
+from recipes.models import Recipe
 
 def index(request):
-    return render(request, "foodspot_app/index.html")
+    categories = Category.objects.all()
+    context ={"categories":categories}
+    return render(request, "foodspot_app/index.html",context)
+
+def recipes(request, category_id):
+    recipes = Recipe.objects.filter(category=category_id)
+    category= Category.objects.get(pk=category_id)
+    context={"recipes":recipes, "category":category}
+    return render(request,"foodspot_app/recipes.html",context)
