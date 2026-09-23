00eea79d7b5fcc75acb9eb3425b1f436bb2d40c9
Christian <kalonjic34@gmail.com>
2026-09-23 14:35:46 +0200

    add URL patterns for category and recipe addition

--- foodspot_app/urls.py
+++ foodspot_app/urls.py
@@ -5,5 +5,7 @@ app_name = "foodspot_app"
 
 urlpatterns = [
     path("",views.index, name="index"),
-    path("recipes/<int:category_id>/",views.recipes, name="recipes")
+    path("recipes/<int:category_id>/",views.recipes, name="recipes"),
+    path("add-category",views.add_category, name="add_category"),
+    path("add_recipe",views.add_recipe, name="add_recipe_no_genre")
 ]
