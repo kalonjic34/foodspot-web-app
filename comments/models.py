2c539a08f9bc232d54d22dbd0b17375b62d20aa8
Christian <kalonjic34@gmail.com>
2026-09-24 16:26:28 +0200

    update Comment model to include user field and improve string representation

--- comments/models.py
+++ comments/models.py
@@ -1,12 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 from recipes.models import Recipe
 
 class Comment(models.Model):
-    recipe=models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    recipe=models.ForeignKey(Recipe, on_delete=models.CASCADE,related_name="comments")
+    user=models.ForeignKey(User, on_delete=models.CASCADE,null=True,related_name="comments")
     text =models.TextField()
     date_added = models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
-        return self.text
+        return f"Commnets by {self.user.username} on {self.recipe.text}"
     
