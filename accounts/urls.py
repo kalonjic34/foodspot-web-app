74e4e18a358f25c99eec7b90e1899d3996c39408
Christian <kalonjic34@gmail.com>
2026-09-24 17:51:07 +0200

    add edit profile URL for user profile editing

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,11 @@
+from accounts import views
+
 from . views import register
 
 from django.urls import path, include
 app_name ="accounts"
 urlpatterns = [
     path("register/", register, name="register"),
-    path("", include("django.contrib.auth.urls"))
+    path("", include("django.contrib.auth.urls")),
+    path("edit-profile/", views.edit_user_profile, name="edit_user_profile")
 ]
