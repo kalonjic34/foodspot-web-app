3ee3d57398717d68235cb09b4ced5b8d63564db1
Christian <kalonjic34@gmail.com>
2026-09-24 17:52:05 +0200

    add static media URL configuration to urlpatterns

--- foodspot/urls.py
+++ foodspot/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls.static import static
+
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,4 +28,4 @@ urlpatterns = [
     path("comments/",include("comments.urls")),
     path("accounts/",include("accounts.urls"))
     
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
