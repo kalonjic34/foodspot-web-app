6e3b20522bb91306ed38bd731f62338c3e71c8ab
Christian <kalonjic34@gmail.com>
2026-09-25 13:47:32 +0200

    add CommentForm for recipe comments

---
+++ comments/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from .models import Comment
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model=Comment
+        fields=["text"]
+        labels={"text":""}
+        widgets={
+            "text":forms.Textarea(
+                attrs={"class":"form-control", "placeholder": "i love this recipe <3"}
+            )
+        }
