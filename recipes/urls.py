7ad2039646f3b51f5a61414ec560184da08622d9
Christian <kalonjic34@gmail.com>
2026-09-25 14:30:01 +0200

    update recipe detail view target

--- recipes/urls.py
+++ recipes/urls.py
@@ -7,6 +7,6 @@ from recipes import views
 app_name = "recipes"
 
 urlpatterns = [
-    path("",views.recipes, name="index"),
-    path("<int:recipe_id>",views.recipe, name="recipe_detail")
+    path("", views.recipes, name="index"),
+    path("<int:recipe_id>", views.recipe_detail, name="recipe_detail")
 ]
